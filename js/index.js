a2bbd7c7239ab4a85e845e2469ae3cf758d00bc9
Abhaj Khan <abhajkhan02@gmail.com>
2024-08-22 07:30:25 +0530

    Iteration1: names and inputs-done

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
-// Iteration 1: Names and Input
+console.log("I'm ready!"); 
+
+ // Iteration 1: Names and Input
+ const hacker1 = 'abhajkhan';
+ console.log('The driver\'s name is '+hacker1);
+ const hacker2 = 'webdevopen';
+ console.log("The navigator's name is "+hacker2);
 //
 // Iteration 2: Conditionals
 
